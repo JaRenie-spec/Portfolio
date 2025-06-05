3af28d724eab3387300dd944b4300247769bb05a
KillianRipoche <killian.ripoche@gmail.com>
2025-06-05 16:39:57 +0200

    add userRoutes

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -1,6 +1,9 @@
 // back/src/app.ts
 import express from 'express';
 import bookRoutes from './routes/book.routes';
+import userRoutes from './routes/user.routes';
+import { userInfo } from 'os';
+import { PrismaClientUnknownRequestError } from '@prisma/client/runtime/library';
 // Importe d'autres routes au besoin
 
 const app = express();
@@ -8,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
+app.use('/users', userRoutes);
 app.use('/api/books', bookRoutes);
 
 export default app;
