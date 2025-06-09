06b652a1a239ac4929602565c6ea8a3cf47f41b6
KillianRipoche <killian.ripoche@gmail.com>
2025-06-09 17:19:02 +0200

    Adding Controller for updateUser

--- back_end/src/controllers/user.controller.ts
+++ back_end/src/controllers/user.controller.ts
@@ -9,3 +9,14 @@ export const createUser = async (req: Request, res: Response) => {
 	const user = await userService.create(data);
 	res.status(201).json(user);
 };
+
+export const updateUser = async (req: Request, res: Response) => {
+	try {
+		const userId = req.params.id; // récupère l'ID depuis l'URL
+		const updatedUser = await userService.update(userId, req.body);
+		return res.status(200).json(updatedUser);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'Erreur lors de la mise à jour' });
+	}
+};
