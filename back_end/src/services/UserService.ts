a7c2e31b866e47c0dd9c87464bdc2e0d7a996a5e
KillianRipoche <killian.ripoche@gmail.com>
2025-06-09 17:11:31 +0200

    Adding async update for the user

--- back_end/src/services/UserService.ts
+++ back_end/src/services/UserService.ts
@@ -1,8 +1,12 @@
-import { CreateUserDTO } from '../types/user.types';
-import axios from 'axios';
-import { KeycloakTokenResponse } from 'types/keycloak.types';
+import { CreateUserDTO } from "../types/user.types";
+import { UpdateUserDTO } from "../types/user.types";
+import { KeycloakTokenResponse } from "types/keycloak.types";
+import axios from "axios";
 /**
  * Service pour gérer les utilisateurs avec Keycloak
+ * Axios est une bibliothèques qui permet de faire des requêtes HTTP ( GET, POST, PUT, DELETE ) simplement.
+ * Elle est utiliser pour communiquer avec des API, récupérer des données ou en envoyer :D
+ * Le payload désigne les données envoyées dans une requête HTTP
  */
 export class UserService {
   async create(data: CreateUserDTO) {
@@ -10,12 +14,12 @@ export class UserService {
     const tokenRes = await axios.post<KeycloakTokenResponse>(
       `${process.env.KEYCLOAK_BASE_URL}/realms/master/protocol/openid-connect/token`,
       new URLSearchParams({
-        client_id: 'admin-cli',
-        grant_type: 'password',
+        client_id: "admin-cli",
+        grant_type: "password",
         username: process.env.KEYCLOAK_ADMIN_USER!,
         password: process.env.KEYCLOAK_ADMIN_PASS!,
       }),
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
     );
 
     const accessToken = tokenRes.data.access_token;
@@ -31,7 +35,7 @@ export class UserService {
         lastName: data.lastName,
         credentials: [
           {
-            type: 'password',
+            type: "password",
             value: data.password,
             temporary: false,
           },
@@ -40,14 +44,67 @@ export class UserService {
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     );
 
     return {
       email: data.email,
-      message: 'Utilisateur créé dans Keycloak',
+      message: "Utilisateur créé dans Keycloak",
+    };
+  }
+
+  async update(id: string, data: UpdateUserDTO) {
+    const tokenRes = await axios.post<KeycloakTokenResponse>(
+      `${process.env.KEYCLOAK_BASE_URL}/realms/master/protocol/openid-connect/token`,
+      new URLSearchParams({
+        client_id: "admin-cli",
+        grant_type: "password",
+        username: process.env.KEYCLOAK_ADMIN_USER!,
+        password: process.env.KEYCLOAK_ADMIN_PASS!,
+      }),
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
+
+    const accessToken = tokenRes.data.access_token;
+
+    const payload: any = {
+      ...(data.firstName && { firstName: data.firstName }),
+      ...(data.lastName && { lastName: data.lastName }),
+      ...(data.email && { email: data.email, username: data.email }),
+    };
+
+    await axios.put(
+      `${process.env.KEYCLOAK_BASE_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${id}`,
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (data.password) {
+      await axios.put(
+        `${process.env.KEYCLOAK_BASE_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${id}/reset-password`,
+        {
+          type: "password",
+          value: data.password,
+          temporary: false,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return {
+      message: "Utilisateur mis à jour avec succès !",
     };
   }
 }
