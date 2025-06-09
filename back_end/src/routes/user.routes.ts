30ba0295e85264a5c7e3667ccca90f1e72871a78
KillianRipoche <killian.ripoche@gmail.com>
2025-06-09 20:26:01 +0200

    Adding routes for updateUser

--- back_end/src/routes/user.routes.ts
+++ back_end/src/routes/user.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { createUser } from '../controllers/user.controller';
+import {
+  createUser,
+  updateUser,
+  updateOwnProfile,
+} from '../controllers/user.controller';
 import { validateBody } from '../middlewares/validateBody';
-import { CreateUserSchema } from '../types/user.types';
+import { CreateUserSchema, UpdateUserSchema } from '../types/user.types';
+import { protect } from '../middlewares/protect';
 
 const router = Router();
 
 router.post('/', validateBody(CreateUserSchema), createUser);
+router.put('/:id', validateBody(UpdateUserSchema), updateUser);
+router.put('/me', protect, validateBody(UpdateUserSchema), updateOwnProfile);
 
 export default router;
