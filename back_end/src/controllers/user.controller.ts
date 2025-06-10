1114b714ea40f8994857ec1f79c6939fb52b8c1f
KillianRipoche <killian.ripoche@gmail.com>
2025-06-10 10:28:08 +0200

    Adding deleteUser and deleteOwnUser

--- back_end/src/controllers/user.controller.ts
+++ back_end/src/controllers/user.controller.ts
@@ -25,10 +25,7 @@ export const updateUser = async (req: AuthenticatedRequest, res: Response) => {
   }
 };
 
-export const updateOwnProfile = async (
-  req: AuthenticatedRequest,
-  res: Response
-) => {
+export const updateOwnProfile = async (req: AuthenticatedRequest, res: Response) => {
   try {
     const userId = req.user?.sub;
     if (!userId) return void res.status(401).json({ error: "Non autorisé" });
@@ -39,5 +36,32 @@ export const updateOwnProfile = async (
     return void res
       .status(500)
       .json({ error: "Erreur lors de la mise à jour du profil" });
+  }
+};
+
+// Suppression d'un compte par un admin
+
+export const deleteUser = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.params.id;
+    await userService.delete(userId);
+    return void res.status(200).json({ message: 'Utilisateur supprimé '});
+  } catch (error) {
+    return void res.status(500).json({ error: 'Erreur lors de la suppression' });
+  }
+};
+
+// Suppression de son propre compte
+
+export const deleteOwnAccount = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.user?.sub;
+    if (!userId)
+      return void res.status(401).json({ error: 'Non autorisé' });
+
+    await userService.delete(userId);
+    return void res.status(200).json({ message: 'Compte supprimé' });
+  } catch (error) {
+    return void res.status(500).json({ error: 'Erreur lors de la suppression de votre compte' });
   }
 };
