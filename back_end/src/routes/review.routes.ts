b239b5450db2c6dbc02432bd439c29f68608524c
KillianRipoche <killian.ripoche@gmail.com>
2025-06-11 15:41:52 +0200

    Adding routes for review

--- back_end/src/routes/review.routes.ts
+++ back_end/src/routes/review.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createReview } from '..controllers/review.controller';
+import { createReview } from '../controllers/review.controller';
 import { validateBody } from 'middlewares/validateBody';
 import { CreateReviewSchema } from 'types/review.types';
 import { protect } from 'middlewares/protect';
