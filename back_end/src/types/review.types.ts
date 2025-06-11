8068ee9e8edebc8a9cf10fce40b97ce4b8aa20c1
KillianRipoche <killian.ripoche@gmail.com>
2025-06-11 15:42:23 +0200

    Adding review types

--- back_end/src/types/review.types.ts
+++ back_end/src/types/review.types.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
 export const CreateReviewSchema = z.object({
-	bookId: z.string().uuid({ message: 'bookId doit être un UUID validé' }),
-	comment: z.string().min(3, { message: 'Le commentaire doit faire au moins 3 caractères' }),
-	rating: z
-		.number()
-		.int({ message: 'la note doit être un nombre entier '})
-		.min(1, { message: 'La note doit être au moins de 1'})
-		.max(5, { message: 'la note doit être au maximum 5 !'}),
+	comment: z.string().min(1),
+	rating: z.number().min(1).max(5),
+	bookId: z.string().uuid(),
+	userId: z.string().uuid().optional(),
+	authorId: z.string().uuid().optional(),
 });
 
 export type CreateReviewDTO = z.infer<typeof CreateReviewSchema>;
