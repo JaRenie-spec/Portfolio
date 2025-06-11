b105e5dc6c7e1ebe741cf83de476936d58bfc817
KillianRipoche <killian.ripoche@gmail.com>
2025-06-11 15:42:06 +0200

    Adding ReviewService

--- back_end/src/services/ReviewService.ts
+++ back_end/src/services/ReviewService.ts
@@ -2,27 +2,23 @@ import { CreateReviewDTO } from '../types/review.types';
 import prisma from '../../prisma/client';
 
 export class ReviewService {
-	/**
-	 *
-	 */
-	async create(data: CreateReviewDTO & { userId: string }) {
-		const book = await prisma.book.findUnique({
-			where: { id: data.bookId },
-		});
+  public async create(data: CreateReviewDTO & { userId: string }): Promise<any> {
+    const book = await prisma.book.findUnique({
+      where: { id: data.bookId },
+    });
+    if (!book) {
+      throw new Error('Livre introuvable');
+    }
 
-		if (!book) {
-			throw new Error('Livre introuvable');
-		}
-
-		const review = await prisma.review.create({
-			data: {
-				userId: data.userId,
-				bookId: data.bookId,
-				comment: data.comment,
-				rating: data.rating,
-			},
-		});
-
-		return review;
-	}
+    const review = await prisma.review.create({
+      data: {
+        comment: data.comment,
+        rating: data.rating,
+        bookId: data.bookId,
+        userId: data.userId,
+        authorId: data.authorId,
+      },
+    });
+    return review;
+  }
 }
