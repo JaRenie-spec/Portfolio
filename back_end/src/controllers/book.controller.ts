4d5631591cdaa37545ff093d903b2f67b33a85a0
Albat93 <albat93120@gmail.com>
2025-06-13 09:56:15 +0200

    Remove book controller and service files

--- back_end/src/controllers/book.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { getAllBooks } from '../services/book.service';
-
-export async function getBooks(req: Request, res: Response) {
-  const books = await getAllBooks();
-  res.json(books);
-}
