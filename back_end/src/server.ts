23d9c0ade03e2037b62ddd696f3119b4b01537dd
KillianRipoche <killian.ripoche@gmail.com>
2025-06-20 14:21:10 +0200

    feat: simplifier le code du serveur en supprimant la configuration dotenv et en améliorant les messages de démarrage

--- back_end/src/server.ts
+++ back_end/src/server.ts
@@ -1,13 +1,6 @@
-// back/src/server.ts
-import dotenv from 'dotenv';
-dotenv.config();
-
 import app from './app';
-
 const PORT = process.env.PORT || 3000;
-
-// Lancement du serveur
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-  console.log(`Documentation Swagger disponible sur http://localhost:${PORT}/api-docs`);
+  console.log(` Backend démarré → http://localhost:${PORT}`);
+  console.log(` Swagger UI → http://localhost:${PORT}/api-docs`);
 });
