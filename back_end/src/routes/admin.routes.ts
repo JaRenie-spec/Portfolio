6bb5d64e4e1fd60e301c04d539190d384565a84d
KillianRipoche <killian.ripoche@gmail.com>
2025-06-20 14:23:32 +0200

    refactor: réorganiser les routes admin pour améliorer la lisibilité

--- back_end/src/routes/admin.routes.ts
+++ back_end/src/routes/admin.routes.ts
@@ -10,30 +10,13 @@ import { requireRole } from "../middlewares/requireRole";
 
 const router = Router();
 
-router.post(
-  "/",
-  requireRole(["admin", "superAdmin"]),
-  createAdminHandler
-);
-router.get(
-  "/",
-  requireRole(["admin", "superAdmin"]),
-  getAllAdminsHandler
-);
-router.get(
-  "/:id",
-  requireRole(["admin", "superAdmin"]),
-  getAdminByIdHandler
-);
-router.put(
-  "/:id",
-  requireRole(["admin", "superAdmin"]),
-  updateAdminHandler
-);
-router.delete(
-  "/:id",
-  requireRole(["admin", "superAdmin"]),
-  deleteAdminHandler
-);
+router.get("/", requireRole(["admin", "superAdmin"]), getAllAdminsHandler);
+router.get("/:id", requireRole(["admin", "superAdmin"]), getAdminByIdHandler);
+
+router.post("/", requireRole(["admin", "superAdmin"]), createAdminHandler);
+
+router.put("/:id", requireRole(["admin", "superAdmin"]), updateAdminHandler);
+
+router.delete("/:id", requireRole(["admin", "superAdmin"]), deleteAdminHandler);
 
 export default router;
