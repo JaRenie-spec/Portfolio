318cddf680f37c1ea7a773107a8efcb20f49bade
KillianRipoche <killian.ripoche@gmail.com>
2025-06-20 14:20:46 +0200

    add routes

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -1,60 +1,46 @@
-// src/app.ts
-
-import express, { Request, Response } from 'express';
+// FICHIER : src/app.ts
+import express from 'express';
+import cors from 'cors';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-
-import bookRoutes from './routes/book.routes';
-import purchaseRoutes from './routes/purchase.routes';
+import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
-import reviewRoutes from './routes/review.routes';
-import eventRoutes from './routes/event.routes';
 import authorRoutes from './routes/author.routes';
-import adminRoutes from './routes/admin.routes';
+import bookRoutes from './routes/book.routes';
+import eventRoutes from './routes/event.routes';
+import reviewRoutes from './routes/review.routes';
+import purchaseRoutes from './routes/purchase.routes';
 
+dotenv.config();
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// 1) Accueil redirige vers Swagger UI
-
-app.get('/', (_req: Request, res: Response) => {
-  return res.redirect('/api-docs');
-});
-
-// 2) Configuration Swagger
 const swaggerSpec = swaggerJsdoc({
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Admin API',
+      title: 'API eBook Store',
       version: '1.0.0',
-      description: 'Documentation Swagger de l’API Admin'
+      description: 'Documentation Swagger de l’API eBook Store',
     },
-    servers: [
-      { url: 'http://localhost:3000' }
-    ],
+    servers: [{ url: 'http://localhost:3000' }],
     components: {
       securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT'
-        }
+        bearerAuth: { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }
       }
     },
-    security: [{ bearerAuth: [] }]
+    security: [{ bearerAuth: [] }],
   },
-  apis: ['./src/routes/*.ts']
+  apis: ['./src/routes/*.ts'],
 });
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// 3) Montage des routes
 app.use('/api/users', userRoutes);
+app.use('/api/authors', authorRoutes);
 app.use('/api/books', bookRoutes);
-app.use('/api/purchases', purchaseRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/reviews', reviewRoutes);
-app.use('/api/authors', authorRoutes);
-app.use('/api/admins', adminRoutes);
+app.use('/api/purchases', purchaseRoutes);
 
 export default app;
