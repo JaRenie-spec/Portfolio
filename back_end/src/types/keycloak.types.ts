4fbebcf69af6f7dca54fb9232ee84c1bfabdf095
KillianRipoche <killian.ripoche@gmail.com>
2025-06-24 13:38:04 +0200

    fix: supprimer les propriétés obsolètes de l'interface KeycloakTokenResponse

--- back_end/src/types/keycloak.types.ts
+++ back_end/src/types/keycloak.types.ts
@@ -6,10 +6,11 @@ export interface KeycloakTokenResponse {
 	access_token: string;
 	expires_in: number;
 	refresh_expires_in: number;
-	refresh_token: string;
 	token_type: string;
-	id_token?: string;
-	not_before_policy: number;
-	session_state: string;
 	scope: string;
+}
+
+interface KeycloakRole {
+	id: string;
+	name: string;
 }
